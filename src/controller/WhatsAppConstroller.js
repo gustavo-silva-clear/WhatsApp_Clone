59b0ab8fcad815b6340df83edc6cc3d5375a424c
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-03-07 10:22:55 -0300

    feat: starting the project by creating the App's main control

---
+++ src/controller/WhatsAppConstroller.js
@@ -0,0 +1,21 @@
+class WhatsAppConstroller {
+
+    constructor() {
+
+        this.loadElements();
+    }
+
+    loadElements() {
+
+        this.el = {};
+
+        document.querySelectorAll('[id]').forEach(element=>{
+
+            this.el[Format.getCamelCase(element.id)] = element;
+
+
+        });
+
+    }
+
+}
