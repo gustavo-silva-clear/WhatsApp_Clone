0444b3621b08fef1e8511a23f6cf4a813645202b
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-03-08 10:38:49 -0300

    feat: inserting Emoji with getSelection(), createRange() and DocumentFragment()

--- src/controller/WhatsAppConstroller.js
+++ src/controller/WhatsAppConstroller.js
@@ -365,6 +365,29 @@ class WhatsAppConstroller {
 
                 this.el.inputText.appendChild(img);
 
+                let cursor = window.getSelection();
+
+                if(!cursor.focusNode || !cursor.focusNode.id == 'input-text'){
+
+                    this.el.inputText.focus();
+
+                    cursor = window.getSelection();
+
+                }
+
+                let range = document.createRange();
+
+                range = cursor.getRangeAt(0);
+                range.deleteContents();
+
+                let frag = document.createDocumentFragment();
+
+                frag.appendChild(img);
+
+                range.insertNode(frag);
+
+                range.setStartAfter(img);
+
                 this.el.inputText.dispatchEvent(new Event('keyup'));
 
             });
