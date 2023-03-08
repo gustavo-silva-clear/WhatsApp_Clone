555d0c86c1ce1c7d32acf7a88bdc27dce9171f99
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-03-08 12:51:46 -0300

    feat: Take camera picture with canvas

--- src/controller/cameraController.js
+++ src/controller/cameraController.js
@@ -7,7 +7,8 @@ export class cameraController {
         navigator.mediaDevices.getUserMedia({
             video: true
         }).then(stream =>{
-
+           
+            this._stream =  stream;
             this._videoEl.srcObject = new MediaStream(stream);
             this._videoEl.play();
 
@@ -19,5 +20,29 @@ export class cameraController {
 
     }
 
+    stop(){
+
+        this._stream.getTracks().forEach(tracks=>{
+
+            tracks.stop();
+
+        });
+
+    }
+
+    takePicture(mimetype = 'image/png'){
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height' , this._videoEl.videoHeight);
+        canvas.setAttribute('width' , this._videoEl.videoWidth);
+
+        let context = canvas.getContext('2d');
+
+        context.drawImage(this._videoEl ,0,0,canvas.width , canvas.height);
+
+        return canvas.toDataURL(mimetype);
+
+    }
 
 }
